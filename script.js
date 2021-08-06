ff5730fa79725b47e681d16d0de02a5bd8c2e93f
Tobi <olanipekunglory4sucess@gmail.com>
2021-08-06 21:23:52 +0200

    added a console.log

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-window.addEventListener('keydown', playSound);
+window.addEventListener("keydown", playSound);
 function playSound(e) {
-        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-        const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-        console.log(key);
-        if(!audio) return;
-        audio.currentTime = 0;
-        audio.play();
-        key.classList.add('playing')
-        // key.classList.remove('playing')
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  console.log(key);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("playing");
+  // key.classList.remove('playing')
 }
-function removeTransition (e) {
-
-    if(e.propertyName !== 'transform') return;
-    this.classList.remove('playing');
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
 }
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+console.log("fdkifkf");
